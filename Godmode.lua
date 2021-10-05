d4b41c827fff2b8340380631b83fc89bfd5b0c73
antonypadela <71530505+sans-undertae@users.noreply.github.com>
2021-10-05 10:02:07 -0400

    Update Godmode.lua
    Comment detailing some bugs.

--- Godmode.lua
+++ Godmode.lua
@@ -1,1 +1,7 @@
+-- Extremely weird
+-- Give you NaN health, blacking out your healthbar on teammate's screens
+-- Melee enemies completely ignore you, and just walk past you.
+-- You can not set off any traps, like snares and dynamites.
+-- Resetting will permenantly break your screen, softlocking the server until you rejoin.
+
 workspace.ServerStuff.dealDamage:FireServer({"meleedamage",game.Players.LocalPlayer.Character,tonumber("NaN"),false,"PLBlade",false,false,{},game.Players.LocalPlayer.Character.Head}, nil, getrenv()._G.pseed[getrenv()._G.list[workspace.ServerStuff.ask:InvokeServer("main", true)]])
