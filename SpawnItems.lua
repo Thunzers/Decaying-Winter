1bab31dc3460b9f088e47de94d7789a83994531a
antonypadela <71530505+sans-undertae@users.noreply.github.com>
2021-10-04 22:53:35 -0400

    Update SpawnItems.lua

--- SpawnItems.lua
+++ SpawnItems.lua
@@ -1,7 +1,4 @@
-local ToSpawn = "lBuP"
+local ToSpawn = "JBox"
     
 local Key = getrenv()._G.pseed[getrenv()._G.list[workspace.ServerStuff.ask:InvokeServer("main", true)]]
 workspace.ServerStuff.throwWeapon:FireServer(ToSpawn, nil, CFrame.new(), "drop", {}, 2e9, false, Key)
-local Wep = game:GetService("Workspace").WeaponDrops:WaitForChild(ToSpawn)--[[
-workspace.ServerStuff.claimItem:InvokeServer(Wep)
-workspace.ServerStuff.getTPWeapon:FireServer(ToSpawn, game:GetService("ReplicatedStorage").animationSets.TPanimSets["2HSPR"], "Fist", Wep, false)--]]
