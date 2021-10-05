70b651f8d2f2b2ecce7b0249b699999d1f9d391e
antonypadela <71530505+sans-undertae@users.noreply.github.com>
2021-10-05 10:00:15 -0400

    Update DEV_KeyGen.lua
    Comment for context

--- DEV_KeyGen.lua
+++ DEV_KeyGen.lua
@@ -1,1 +1,6 @@
+-- Simpily generates a key many remotes use
+-- For context, most of the "important" remotes have an argument, of a randomly generated number between -10000 and 10000
+-- Using the wrong number, or no number at all, will instantly ban you.
+-- This line of code simpily generates that key, preventing a ban.
+
 local Key = getrenv()._G.pseed[getrenv()._G.list[workspace.ServerStuff.ask:InvokeServer("main", true)]]
